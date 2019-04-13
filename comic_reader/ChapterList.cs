950fd5c43dbb1ab1f382ad3bad6e9a32002c1112
Merlin Nguyễn <jonhenry13241@gmail.com>
2019-04-13 15:17:57 +0700

    initial commit

---
+++ comic_reader/ChapterList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace comic_reader
+{
+    public static class ComicCollection
+    {
+        public static List<string> Comics { get; set; }
+
+        public static Dictionary<string, List<string>> ComicsPages { get; set; }
+
+        public static void Init()
+        {
+            Comics = Directory.GetDirectories("comic").ToList();
+            ComicsPages = new Dictionary<string, List<string>>();
+            foreach (var comic in Comics)
+            {
+                var ext = new List<string> { ".jpg", ".png", ".gif" };
+                var images = Directory.GetFiles(comic, "*.*", SearchOption.AllDirectories)
+                     .Where(s => ext.Contains(Path.GetExtension(s)));
+                ComicsPages.Add(comic, images.ToList());
+            }
+        }
+    }
+}
---
+++ comic_reader/Pages/Viewer.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace comic_reader.Pages
+{
+    public class ViewerModel : PageModel
+    {
+        public string Chapter;
+        public string CurrentPage;
+        public string NextPage;
+        public string PreviousPage;
+        public void OnGet()
+        {
+            this.Chapter = this.HttpContext.Request.Query["chapter"];
+            int.TryParse(this.HttpContext.Request.Query["curr"], out var currPage);
+            var prevPage = currPage - 1;
+            var nextPage = currPage + 1;
+
+            var chapter = ComicCollection.ComicsPages[this.Chapter];
+            {
+                var imageUrl = $"\\static\\{chapter[currPage]}\"".Replace('\\', '/');
+                this.CurrentPage = imageUrl;
+            }
+            if (currPage == 0)
+            {
+                prevPage = -1;
+            }
+            if (currPage == chapter.Count - 1)
+            {
+                nextPage = -1;
+            }
+
+            if (prevPage != -1)
+            {
+                this.PreviousPage = $"/viewer?chapter={this.Chapter}&curr={prevPage}";
+            }
+            else
+            {
+                this.PreviousPage = "";
+            }
+
+            if (nextPage != -1)
+            {
+                this.NextPage = $"/viewer?chapter={this.Chapter}&curr={nextPage}";
+            }
+            else
+            {
+                this.NextPage = "";
+            }
+        }
+    }
+}
